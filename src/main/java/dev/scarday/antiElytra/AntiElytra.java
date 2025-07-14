4ba4dbebb97479eb71486818666ab08a39ba0fbe
ScarDay <alexeyishutov@gmail.com>
2025-07-14 06:22:21 +0400

    Init commit

---
+++ src/main/java/dev/scarday/antiElytra/AntiElytra.java
@@ -0,0 +1,16 @@
+package dev.scarday.antiElytra;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class AntiElytra extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
